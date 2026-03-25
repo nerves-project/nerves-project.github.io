5d0f1a094b6a59ec984807c2e6e1b670a7d57e2b
Lars Wikman <lars@underjord.io>
2026-03-25 10:06:08 +0100

    Change video layout to improve visual interest
    Lets us feature a video per year. Not because that's a priority but
    because it gives people some guidance.

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,30 +1,28 @@
 export default {
   init () {
     document.addEventListener('DOMContentLoaded', () => {
-      // Get all "dropdown" elements
-      const $tabs = Array.prototype.slice.call(document.querySelectorAll('#video-container div.tabs li'), 0);
-      const $contents = Array.prototype.slice.call(document.querySelectorAll('#video-container div[id*="-content"]'))
-    
-      // Add a click event on each of them
+      const $tabs = Array.prototype.slice.call(document.querySelectorAll('#video-container .video-tab'), 0);
+      const $contents = Array.prototype.slice.call(document.querySelectorAll('#video-container div[id$="-content"]'));
+
       $tabs.forEach( el => {
         el.addEventListener('click', () => {
-    
+
           $tabs.forEach( tab => {
             tab.classList.remove('is-active');
           });
           $contents.forEach( content => {
-            if (content.id === `${el.children[0].id}-content`) {
-              content.style.display = 'flex';
+            if (content.id === `${el.id}-content`) {
+              content.style.display = 'block';
             } else {
               content.style.display = 'none';
             }
           });
-    
-          el.classList.toggle('is-active');
-          
+
+          el.classList.add('is-active');
+
         });
       });
-    
+
     });
   }
 }
